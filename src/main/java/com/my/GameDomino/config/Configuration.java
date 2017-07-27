4cad21064832738879a81504fdb6eab4c4a047b9
Vitalii <8imposible8@gmail.com>
2017-07-27 16:46:48 +0300

    Update Configuration.java

--- src/main/java/com/my/GameDomino/config/Configuration.java
+++ src/main/java/com/my/GameDomino/config/Configuration.java
@@ -6,54 +6,54 @@ import java.util.Properties;
 
 public class Configuration {
 
-	private static Configuration instance;
-	private Connection connection;
+    private static Configuration instance;
+    private Connection connection;
 
-	private Configuration() {
+    private Configuration() {
 
-	}
+    }
 
-	public static Configuration getInstance() {
-		if (instance == null) {
-			instance = new Configuration();
-		}
-		return instance;
-	}
+    public static Configuration getInstance() {
+        if (instance == null) {
+            instance = new Configuration();
+        }
+        return instance;
+    }
 
-	public Connection getConnection() {
-		if (connection != null) {
-			return connection;
-		}
-		String path = "application.properties";
-		ClassLoader loader = Thread.currentThread().getContextClassLoader();
-		Properties properties = new Properties();
-		try (InputStream resourceStream = loader.getResourceAsStream(path)) {
-			properties.load(resourceStream);
+    public Connection getConnection() {
+        if (connection != null) {
+            return connection;
+        }
+        String path = "application.properties";
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        Properties properties = new Properties();
+        try (InputStream resourceStream = loader.getResourceAsStream(path)) {
+            properties.load(resourceStream);
 
-			String driver = properties.getProperty("driver");
-			String url = properties.getProperty("url");
-			String user = properties.getProperty("user");
-			String password = properties.getProperty("password");
+            String driver = properties.getProperty("driver");
+            String url = properties.getProperty("url");
+            String user = properties.getProperty("user");
+            String password = properties.getProperty("password");
 
-			Class.forName(driver);
-			connection = DriverManager.getConnection(url, user, password);
-			System.out.println("Successful connection.");
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.out.println("Failed connection:" + e.getMessage());
-		}
+            Class.forName(driver);
+            connection = DriverManager.getConnection(url, user, password);
+            System.out.println("Successful connection.");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Failed connection:" + e.getMessage());
+        }
 
-		return connection;
-	}
+        return connection;
+    }
 
-	public void closeConnection(Connection toBeClosed) {
-		if (toBeClosed == null)
-			return;
-		try {
-			toBeClosed.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
+    public void closeConnection(Connection toBeClosed) {
+        if (toBeClosed == null)
+            return;
+        try {
+            toBeClosed.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
