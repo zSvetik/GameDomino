a4eb30629f02c174b1659cffa9cf329409976dbe
Vitalii <8imposible8@gmail.com>
2017-07-27 16:42:48 +0300

    Update StartGame.java

--- src/main/java/com/my/GameDomino/StartGame.java
+++ src/main/java/com/my/GameDomino/StartGame.java
@@ -4,11 +4,11 @@ import com.my.GameDomino.server.JettyServer;
 
 public class StartGame {
 
-	public static void main(String[] args) throws Exception {
-		int port = 8080;
-		JettyServer server = new JettyServer(port);
-		server.start();
-		server.waitForInterrupt();
-	}
+    public static void main(String[] args) throws Exception {
+        int port = 8080;
+        JettyServer server = new JettyServer(port);
+        server.start();
+        server.waitForInterrupt();
+    }
 
 }
